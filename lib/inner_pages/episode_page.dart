297474f66290f15236f101e82cb46638b1c0c9df
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-01-11 23:12:44 +0100

    refactoring

--- lib/inner_pages/episode_page.dart
+++ lib/inner_pages/episode_page.dart
@@ -14,73 +14,73 @@ class EpisodePage extends HookWidget {
   final int id;
   final String title;
 
-  @override
-  Widget build(BuildContext context) {
-    final readCharacters = useQuery(QueryOptions(
-      document: gql(singleEpisodeCharactersGraphQl),
-      fetchPolicy: FetchPolicy.noCache,
-      variables: {'id': id},
-    ));
-
-    final result = readCharacters.result;
-
-    if (result.hasException) {
-      return Text(result.exception.toString());
-    }
-
-    if (result.isLoading) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: const Center(
-          child: RMCircullarProgressIndicator(),
-        ),
-      );
-    }
-
+  Widget _getBody(QueryResult<Object?> result, BuildContext context) {
     final characterList = result.data?['episode']?['characters'];
 
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250, mainAxisExtent: 250),
+      itemCount: characterList.length,
+      itemBuilder: (_, index) => Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              color: Theme.of(context).cardColor),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 125,
+                  height: 125,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(characterList[index]["image"]),
+                        fit: BoxFit.cover),
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Parameter(
+                    characterList: characterList, index: index, p: "name"),
+                Parameter(
+                    characterList: characterList, index: index, p: "species"),
+                Parameter(
+                    characterList: characterList, index: index, p: "gender"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 250, mainAxisExtent: 250),
-        itemCount: characterList.length,
-        itemBuilder: (_, index) => Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                color: Theme.of(context).cardColor),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                children: [
-                  Container(
-                    width: 125,
-                    height: 125,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(characterList[index]["image"]),
-                          fit: BoxFit.cover),
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  Parameter(
-                      characterList: characterList, index: index, p: "name"),
-                  Parameter(
-                      characterList: characterList, index: index, p: "species"),
-                  Parameter(
-                      characterList: characterList, index: index, p: "gender"),
-                ],
-              ),
-            ),
-          ),
+      body: Query(
+        options: QueryOptions(
+          document: gql(singleEpisodeCharactersGraphQl),
+          fetchPolicy: FetchPolicy.noCache,
+          variables: {'id': id},
         ),
+        builder: (result, {fetchMore, refetch}) {
+          if (result.hasException) {
+            return const Text('There was an issue loading this page');
+          }
+
+          if (result.isLoading) {
+            return const Center(
+              child: RMCircullarProgressIndicator(),
+            );
+          }
+
+          return _getBody(result, context);
+        },
       ),
     );
   }
